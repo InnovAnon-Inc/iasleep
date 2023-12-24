b0a02904c3de2eef56499717c2a667362d2aa985
lmaddox <InnovAnon-Inc@protonmail.com>
2023-12-24 14:47:45 -0600

    backup

--- init.lua
+++ init.lua
@@ -1,3 +1,4 @@
+local MODNAME = minetest.get_current_modname()
 local S = minetest.get_translator("iasleep")
 
 if minetest.settings:get_bool("enable_damage") then
--- sleep.lua
+++ sleep.lua
@@ -1,3 +1,4 @@
+local MODNAME = minetest.get_current_modname()
 -- Keep these for backwards compatibility
 function iasleep.save_sleep(player)
 	iasleep.set_sleep_raw(player)
